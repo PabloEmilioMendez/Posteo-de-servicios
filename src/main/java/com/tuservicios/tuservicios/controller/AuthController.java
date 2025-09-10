4165d5480148e2ffb9c89aed4d047d45f3c8c322
Pablo Emilio Mendez <pmendezpuentes@gmail.com>
2025-09-09 21:53:20 -0500

    Creacion controller AuthController

--- src/main/java/com/tuservicios/tuservicios/controller/AuthController.java
+++ src/main/java/com/tuservicios/tuservicios/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.tuservicios.tuservicios.controller;
 
+import com.tuservicios.tuservicios.payload.request.SingupRequest;
+import com.tuservicios.tuservicios.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class AuthController {
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SingupRequest singupRequest){
+        try {
+            authService.registerUser(singupRequest);
+            return ResponseEntity.ok("Usuario registrado exitosamente");
+        }catch (RuntimeException e){
+            return  ResponseEntity.badRequest().body(e.getMessage());
+
+        }
+
+    }
 }
