5e263a280572e3f1e4b62c3f7a13f8c2b3ffbdcd
Pablo Emilio Mendez <pmendezpuentes@gmail.com>
2025-09-10 23:53:57 -0500

    Creacion metodos para generar el JWT

--- src/main/java/com/tuservicios/tuservicios/controller/AuthController.java
+++ src/main/java/com/tuservicios/tuservicios/controller/AuthController.java
@@ -1,14 +1,18 @@
 package com.tuservicios.tuservicios.controller;
 
+import com.tuservicios.tuservicios.payload.request.LoginRequest;
 import com.tuservicios.tuservicios.payload.request.SingupRequest;
 import com.tuservicios.tuservicios.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins =  "*", maxAge = 3600)
 public class AuthController {
     @Autowired
     AuthService authService;
@@ -20,8 +24,18 @@ public class AuthController {
             return ResponseEntity.ok("Usuario registrado exitosamente");
         }catch (RuntimeException e){
             return  ResponseEntity.badRequest().body(e.getMessage());
-
         }
-
     }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authencateUser(@Valid @RequestBody LoginRequest loginRequest){
+        try {
+            Authentication authentication = authService.authenticateUser(loginRequest);
+            // Se retorna el JWT
+            return  ResponseEntity.ok("Usuario autenticado exitosamente");
+        } catch (Exception e) {
+            return  ResponseEntity.badRequest().body("Credenciales de usuario no validas");
+        }
+    }
+
 }
--- src/main/java/com/tuservicios/tuservicios/service/AuthService.java
+++ src/main/java/com/tuservicios/tuservicios/service/AuthService.java
@@ -1,11 +1,18 @@
 package com.tuservicios.tuservicios.service;
 
 import com.tuservicios.tuservicios.model.User;
+import com.tuservicios.tuservicios.payload.request.LoginRequest;
 import com.tuservicios.tuservicios.payload.request.SingupRequest;
 import com.tuservicios.tuservicios.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
 
     @Autowired
@@ -14,27 +21,33 @@ public class AuthService {
     @Autowired
     PasswordEncoder encoder;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     public User registerUser(SingupRequest singupRequest){
         //Verificamos si el nombre de usuario ya existe
         if(userRepository.existsByUsername(singupRequest.getUsername())) {
             throw new RuntimeException("Error: El nombre del usuario ya está en uso");
         };
-
         //Verificamos si el correo electronico ya existe
         if(userRepository.existsByEmail(singupRequest.getEmail())){
             throw new RuntimeException("Error: El correo electronico ya esta en uso ");
         };
-
         // Crear una nueva cuenta de usuario
         User user = new User();
         user.setUsername(singupRequest.getUsername());
         user.setEmail(singupRequest.getEmail());
-
         //Encriptar la contreseña y guardarla
         String encodedPassword = encoder.encode(singupRequest.getPassword());
         user.setPassword(encodedPassword);
-
         return userRepository.save(user);
+    };
 
+    public Authentication authenticateUser(LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return  authentication;
     }
 }
