a96ecabe9fea8b6a7d339431f6654b87ad66b783
Pablo Emilio Mendez <pmendezpuentes@gmail.com>
2025-09-24 22:44:14 -0500

    Sea crea la entidad BaseEntity general para las demas entidades futuras

---
+++ src/main/java/com/tuservicios/tuservicios/model/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.tuservicios.tuservicios.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    private LocalDateTime fechaDeRegistro;
+
+    private LocalDateTime fechaDeModificacion;
+
+    private Boolean activo = true;
+
+    @PrePersist
+    protected void onCreate(){
+        this.fechaDeRegistro = LocalDateTime.now();
+        this.fechaDeModificacion = LocalDateTime.now();
+    }
+
+    @PrePersist
+    protected  void onUdpate(){
+        this.fechaDeModificacion = LocalDateTime.now();
+    }
+}
