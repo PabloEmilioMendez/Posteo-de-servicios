de52fc01d49a2ebce5af1d2a11767df5fe8c6ea2
Pablo Emilio Mendez <pmendezpuentes@gmail.com>
2025-11-06 07:51:39 -0500

    Se crean los metodos de consultar todos los paises y pais por id

--- src/main/java/com/tuservicios/tuservicios/controller/PaisController.java
+++ src/main/java/com/tuservicios/tuservicios/controller/PaisController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/paises")
@@ -20,5 +23,16 @@ public class PaisController {
     public ResponseEntity<Pais> createCountry(@Valid @RequestBody Pais pais){
         Pais newPais = paisRepository.save(pais);
         return new ResponseEntity<>(newPais, HttpStatus.OK);
+    }
+    //READ ALL COUNTRY
+    @GetMapping
+    public List<Pais> getAllCountry(){
+         return paisRepository.findAll();
+    }
+    //READ ID COUNTRY
+    @GetMapping("{id}")
+    public Pais getCountryById(@PathVariable Long id){
+        return paisRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Pais con no encotrado con ID: " + id));
     }
 }
