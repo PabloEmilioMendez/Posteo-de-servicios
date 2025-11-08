1b2323ff261a254fb82748cf2f56892edf2ce66b
Pablo Emilio Mendez <pmendezpuentes@gmail.com>
2025-11-08 18:23:46 -0500

    Se crea metodo en CiudadRepository para listar las ciudades por ID departamento

--- src/main/java/com/tuservicios/tuservicios/repository/CiudadRepository.java
+++ src/main/java/com/tuservicios/tuservicios/repository/CiudadRepository.java
@@ -3,5 +3,9 @@ package com.tuservicios.tuservicios.repository;
 import com.tuservicios.tuservicios.model.Ciudad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CiudadRepository extends JpaRepository<Ciudad, Long> {
+
+    List<Ciudad> findByDepartamentoId(Long departamentoId);
 }
