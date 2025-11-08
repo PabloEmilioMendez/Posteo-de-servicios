74aca53acb71bd0e6f6b6d409033d093942127f6
Pablo Emilio Mendez <pmendezpuentes@gmail.com>
2025-11-08 16:23:49 -0500

    Se agrega metodo para listar los departamentos por id del pais al que pertenecen

--- src/main/java/com/tuservicios/tuservicios/repository/DepartamentoRepository.java
+++ src/main/java/com/tuservicios/tuservicios/repository/DepartamentoRepository.java
@@ -3,5 +3,9 @@ package com.tuservicios.tuservicios.repository;
 import com.tuservicios.tuservicios.model.Departemento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartamentoRepository extends JpaRepository<Departemento, Long> {
+    //Search for departmensts by the country ID they belong to
+    List<Departemento> findByPaisId(Long paisId);
 }
